9fe7ad55c56f48004fc5c6495fef93ba0a5c46f6
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2016-10-03 12:14:03 +0545

    offer map step definations

---
+++ features/step_definitions/offer_map_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I am a logged in user\.$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I visit the offer index page\.$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I can see the list of offers$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I can see the map with offer marker$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
